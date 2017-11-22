905f79ff666df6d930bc4367433f1dc9678ffab5
Ariel Jakobovits <arieljake@gmail.com>
2017-11-21 20:56:47 -0800

    completion page

--- src/apps/vendor/components/Introduction.vue
+++ src/apps/vendor/components/Introduction.vue
@@ -16,7 +16,7 @@
 
 <template>
 <BasePage pageName="">
-  <div slot="content" id="Questionnaire">
+  <div slot="content">
     <div class="container">
       <div class="row">
         <div class="buttons col-sm-8 offset-sm-2" v-if="questionnaire">
