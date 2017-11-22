b09295d78d0ff1052cefcc34d92df4d45a546773
Ariel Jakobovits <arieljake@gmail.com>
2017-11-21 20:56:47 -0800

    completion page

--- src/apps/vendor/components/Complete.vue
+++ src/apps/vendor/components/Complete.vue
@@ -1,13 +1,42 @@
-
+<style>
+.intro h3 {
+  margin-top: 2rem !important
+}
+</style>
 
 <template>
-<div>
-  COMPLETE
-</div>
+<BasePage pageName="">
+  <div slot="content">
+    <div class="container">
+      <div class="row">
+        <div class="intro col-sm-8 offset-sm-2" v-if="questionnaire" v-html="completionHtml">
+        </div>
+      </div>
+    </div>
+  </div>
+</BasePage>
 </template>
 
 <script>
-export default {
+import { mapGetters } from 'vuex';
+import BasePage from './BasePage';
 
+export default {
+  components: {
+    BasePage
+  },
+  data() {
+    return {
+      markdown: new showdown.Converter()
+    };
+  },
+  computed: {
+    ...mapGetters({
+      questionnaire: 'questionnaire'
+    }),
+    completionHtml() {
+      return this.markdown.makeHtml(this.questionnaire.completion);
+    }
+  }
 };
 </script>
