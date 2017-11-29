8b20802ca71fce0a16364351fa87ec9f8bf8385d
Ariel Jakobovits <arieljake@gmail.com>
2017-11-28 20:48:25 -0800

    debug vendor sort

--- src/lib/data.js
+++ src/lib/data.js
@@ -1,5 +1,7 @@
-export default {
-  genId() {
-    return Math.random().toString().slice(2);
-  }
+export const genId = () => {
+  return Math.random().toString().slice(2);
+};
+
+export const dateFromObjectId = (objectId) => {
+  return new Date(parseInt(objectId.substring(0, 8), 16) * 1000);
 };
