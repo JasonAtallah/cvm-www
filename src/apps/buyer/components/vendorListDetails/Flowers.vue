d13bdb19bbc3aa3876dc12e4e30646e7526bfc27
Jason Atallah <jasonatallah@jasons-mbp.attlocal.net>
2017-12-10 14:19:42 -0800

    added file name and mime icons to product details

--- src/apps/buyer/components/vendorListDetails/Flowers.vue
+++ src/apps/buyer/components/vendorListDetails/Flowers.vue
@@ -47,7 +47,28 @@
         <tr v-if="activeRow === item.name">
           <td colspan="7">
             <div class="card product-additional-info">
-              hi                    
+              <div class="row">
+                <div class="col-md-6">
+                  <h6>Photos</h6>
+                  <span v-if="item.photo.length === 0">No photos were included!</span>                          
+                </div>
+                <div class="col-md-6">
+                  <h6>Test Results</h6>
+                  <span v-if="item.testResults.length === 0">No test results were included!</span>
+                </div>
+              </div>
+              <div class="row">
+                <div class="col-md-6">
+                  <ul class="list-unstyled" v-for="file in item.photo">
+                    <li><i class="fa fa-photo"></i> {{ file.originalname.split('.')[0] }}</li>
+                  </ul>
+                </div>                
+                <div class="col-md-6">
+                  <ul class="list-unstyled" v-for="file in item.testResults">
+                    <li><i class="fa" :class="getFileType(file.mimetype)"></i> {{ file.originalname.split('.')[0] }}</li>
+                  </ul>
+                </div>                
+              </div>              
             </div>
           </td>
         </tr>
@@ -69,6 +90,18 @@ export default {
   methods: {
     showProductDetail(productName) {
       this.$store.commit('productDetailsRow', productName);
+    },
+    getFileType(file) {
+      const fileTypes = {
+        'image/png': 'fa-photo',
+        'image/jpeg': 'fa-photo',
+        'application/pdf': 'fa-file-pdf-o',
+        'application/msword': 'fa fa-file-word-o'
+      };
+      if (fileTypes[file]) {
+        return fileTypes[file];
+      }
+      return 'fa-file-o';
     }
   }
 };
