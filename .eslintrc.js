b894160aa253ce7483af4f92ac6ab67eeaeccfb0
Ariel Jakobovits <arieljake@gmail.com>
2017-12-11 21:37:47 -0800

    remove eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -55,6 +55,7 @@ module.exports = {
     'no-plusplus': 0,
     'no-underscore-dangle': 0,
     'no-useless-escape': 0,
-    'import/prefer-default-export': 0
+    'import/prefer-default-export': 0,
+    'no-mixed-operators': 0
   }
 };
