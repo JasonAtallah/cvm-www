8823daef7f52bd9b0a8b883a1eb0705809377db8
Jason Atallah <jasonatallah@jasons-mbp.attlocal.net>
2018-01-24 14:05:37 -0800

    added preview/edit modes to questionnaire settings

--- src/apps/buyer/components/detail/buyerSettings/Questionnaire.vue
+++ src/apps/buyer/components/detail/buyerSettings/Questionnaire.vue
@@ -1,3 +1,9 @@
+<style scoped>
+a.markdown-link:hover {
+  text-decoration: none;
+}
+</style>
+
 <template>
   <div>
     <h3>Questionnaire</h3>
@@ -11,15 +17,20 @@
           <div class="form-group" :model="newQuestionnaire[page.value]">
             <div class="row">
               <div class="col-sm-12">
+                <ElButton type="text" @click="switchModes">{{ mode }}</ElButton>
+                (<a class="markdown-link" href="https://github.com/adam-p/markdown-here/wiki/Markdown-Cheatsheet" target="_blank">Learn how to use markdown</a>)
+                <div v-if="previewMode" v-html="markdownHtml" />
+                <div v-else>
                 <label class="settings-input-label"></label>
-                <ElInput :id="page.value" v-model="newQuestionnaire[page.value]" placeholder="Enter text using markdown" />
+                <ElInput :id="page.value" v-model="newQuestionnaire[page.value]" placeholder="Enter text using markdown"
+                type="textarea" :rows="20"/>
+                </div>
               </div>
             </div>
           </div>
         </ElTabPane>
       </ElTabs>
     </div>
-
     <div class="modal-footer">
       <button type="button" class="btn btn-lg btn-primary" @click="updateQuestionnaire" :disabled="canNotUpdate">Save</button>
       <button type="button" class="btn btn-lg btn-default" @click="cancel">Cancel</button>
@@ -31,6 +42,7 @@
 <script>
 import { mapGetters } from 'vuex';
 import {
+  Button as ElButton,
   Input as ElInput,
   Tabs as ElTabs,
   TabPane as ElTabPane,
@@ -39,6 +51,7 @@ import {
 export default {
   props: ['buyer', 'buyerSettings', 'questionnaire'],
   components: {
+    ElButton,
     ElInput,
     ElTabs,
     ElTabPane,
@@ -48,23 +61,36 @@ export default {
     canNotUpdate() {
       return _.isEqual(this.questionnaire.introduction, this.newQuestionnaire.introduction) && _.isEqual(this.questionnaire.completion, this.newQuestionnaire.completion);
     },
+    markdownHtml() {
+      return this.markdown.makeHtml(this.newQuestionnaire[this.curTab]);
+    },
     questionnairePages() {
       return _.find(this.buyerSettings.settingsTabs, { value: 'questionnaire' }).options;
-    }
+    },
   },
   data() {
     return {
       curTab: 'introduction',
+      markdown: new showdown.Converter(),
+      mode: 'Preview',
       newQuestionnaire: {
         introduction: this.questionnaire.introduction || null,
         completion: this.questionnaire.completion || null
-      }
+      },
+      previewMode: false
     };
   },
   methods: {
     cancel() {
-      this.newQuestionnaire.introduction = _.cloneDeep(this.questionnaire.introduction);
-      this.newQuestionnaire.completion = _.cloneDeep(this.questionnaire.completion);
+      if (this.curTab === 'introduction') {
+        this.newQuestionnaire.introduction = _.cloneDeep(this.questionnaire.introduction);
+      } else if (this.curTab === 'completion') {
+        this.newQuestionnaire.completion = _.cloneDeep(this.questionnaire.completion);
+      }
+    },
+    switchModes() {
+      this.mode = (this.previewMode ? 'Preview' : 'Edit');
+      this.previewMode = !this.previewMode;
     },
     switchTab(tab) {
       this.curTab = tab.name;
@@ -83,7 +109,7 @@ export default {
       this.$store.dispatch('updateQuestionnaire', page);
       Notification({
         title: 'Success',
-        messages: 'Questionnaire updated',
+        message: `${this.curTab} page updated`,
         type: 'success',
         duration: 2000
       });
@@ -91,7 +117,3 @@ export default {
   }
 };
 </script>
-
-<style>
-
-</style>
