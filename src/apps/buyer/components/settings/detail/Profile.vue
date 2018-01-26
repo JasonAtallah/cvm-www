96bbfc4b2d5ee294608c33a730346f4224c97532
Jason Atallah <jasonatallah@jasons-mbp.attlocal.net>
2018-01-25 22:52:11 -0800

    converted to element and added validation in buyer settings profile

--- src/apps/buyer/components/settings/detail/Profile.vue
+++ src/apps/buyer/components/settings/detail/Profile.vue
@@ -1,3 +1,5 @@
+
+
 <template>
   <div>
     <h3>Profile</h3>
@@ -5,60 +7,60 @@
       Update your personal and company information.
     </p>
     <div class="card card-body bg-light">
-      <div class="form-group">
-        <div class="row">
-          <div class="col-sm-6">
-            <label class="settings-input-label" for="firstName">First Name</label>
-            <ElInput id="firstName" v-model="profile.contact.firstName" placeholder="First" />
-          </div>
-          <div class="col-sm-6">
-            <label class="settings-input-label" for="lastName">Last Name</label>
-            <ElInput id="lastName" v-model="profile.contact.lastName" placeholder="Last" />
-          </div>
-        </div>
-        <br>
-        <div class="row">
-          <div class="col-sm-6">
-            <label class="settings-input-label" for="phone">Phone</label>
-            <ElInput id="phone" v-model="profile.contact.phone" placeholder="Phone" />
-          </div>
-          <div class="col-sm-6">
-            <label class="settings-input-label" for="email">Email</label>
-            <ElInput id="email" v-model="profile.contact.email" placeholder="Email" />
-          </div>
-        </div><br>
-        <div class="row">
-          <div class="col-sm-12">
-            <h4>Company</h4>
-          </div>
-        </div>
-        <br>
-        <div class="row">
-          <div class="col-sm-6">
-            <label class="settings-input-label" for="name">Name</label>
-            <ElInput id="name" v-model="profile.company.name" placeholder="Name" />
-          </div>
-          <div class="col-sm-6">
-            <label class="settings-input-label" for="address">Address</label>
-            <ElInput id="address" v-model="profile.company.address" placeholder="Address" />
-          </div>
-        </div>
-        <br>
-        <div class="row">
-          <div class="col-sm-4">
-            <label class="settings-input-label" for="city">City</label>
-            <ElInput id="city" v-model="profile.company.city" placeholder="City" />
-          </div>
-          <div class="col-sm-4">
-            <label class="settings-input-label" for="state">State</label>
-            <ElInput id="state" v-model="profile.company.state" placeholder="State" />
-          </div>
-          <div class="col-sm-4">
-            <label class="settings-input-label" for="zip">Zip</label>
-            <ElInput id="zip" v-model="profile.company.zip" placeholder="Zip" />
-          </div>
-        </div>
-     </div>
+      <ElForm :model="profile.contact" :rules="rules">
+        <ElCol :span="11">
+          <ElFormItem label="First Name" prop="firstName">
+            <ElInput v-model="profile.contact.firstName" placeholder="First Name" />
+          </ElFormItem>
+        </ElCol>
+        <ElCol :span="1">&nbsp;</ElCol>
+        <ElCol :span="12">
+          <ElFormItem label="Last Name" prop="lastName">
+            <ElInput v-model="profile.contact.lastName" placeholder="Last Name" />
+          </ElFormItem>
+        </ElCol>
+        <ElCol :span="11">
+          <ElFormItem label="Phone" prop="phone">
+            <ElInput v-model="profile.contact.phone" placeholder="Phone" />
+          </ElFormItem>
+        </ElCol>
+        <ElCol :span="1">&nbsp;</ElCol>
+        <ElCol :span="12">
+          <ElFormItem label="Email" prop="email">
+            <ElInput v-model="profile.contact.email" placeholder="Email" />
+          </ElFormItem>
+        </ElCol>
+      </ElForm>
+      <ElForm :model="profile.company" :rules="rules">
+        <ElCol :span="11">
+          <ElFormItem label="Name" prop="name">
+            <ElInput v-model="profile.company.name" placeholder="Name" />
+          </ElFormItem>
+        </ElCol>
+        <ElCol :span="1">&nbsp;</ElCol>
+        <ElCol :span="12">
+          <ElFormItem label="Address" prop="address">
+            <ElInput v-model="profile.company.address" placeholder="Address" />
+          </ElFormItem>
+        </ElCol>
+        <ElCol :span="8">
+          <ElFormItem label="City" prop="city">
+            <ElInput v-model="profile.company.city" placeholder="City" />
+          </ElFormItem>
+        </ElCol>
+        <ElCol :span="1">&nbsp;</ElCol>
+        <ElCol :span="6">
+          <ElFormItem label="State" prop="state">
+            <ElInput v-model="profile.company.state" placeholder="State" />
+          </ElFormItem>
+        </ElCol>
+        <ElCol :span="1">&nbsp;</ElCol>
+        <ElCol :span="8">
+          <ElFormItem label="Zip" prop="zip">
+            <ElInput v-model="profile.company.zip" placeholder="Zip Code" />
+          </ElFormItem>
+        </ElCol>
+      </ElForm>
     </div>
 
     <div class="modal-footer">
@@ -72,12 +74,18 @@
 <script>
 import {
   Button as ElButton,
+  Col as ElCol,
+  Form as ElForm,
+  FormItem as ElFormItem,
   Input as ElInput,
   Notification } from 'element-ui';
 
 export default {
   components: {
     ElButton,
+    ElCol,
+    ElForm,
+    ElFormItem,
     ElInput,
     Notification
   },
@@ -97,6 +105,39 @@ export default {
           phone: this.buyer.profile.contact.phone || null,
           email: this.buyer.profile.contact.email || null
         }
+      },
+      rules: {
+        firstName: [
+          { required: true, message: 'Please Enter First Name', trigger: 'blur' }
+        ],
+        lastName: [
+          { required: true, message: 'Please Enter Last Name', trigger: 'blur' }
+        ],
+        phone: [
+          { required: true, message: 'Please Enter Phone Number', trigger: 'blur' },
+          { min: 7, message: 'Number Should Be At Least 7 Digits', trigger: 'blur' }
+        ],
+        email: [
+          { required: true, message: 'Please Input Email Address', trigger: 'blur' },
+          { type: 'email', message: 'Please Input Valid Email Address', trigger: 'blur,change' }
+        ],
+        name: [
+          { required: true, message: 'Please Enter a Company Name', trigger: 'blur' }
+        ],
+        address: [
+          { required: true, message: 'Please Enter an Address', trigger: 'blur' }
+        ],
+        city: [
+          { required: true, message: 'Please Enter a City', trigger: 'blur' }
+        ],
+        state: [
+          { required: true, message: 'Please Enter a State', trigger: 'blur' },
+          { min: 2, max: 2, message: 'State Must Be Two Letters', trigger: 'blur' }
+        ],
+        zip: [
+          { required: true, message: 'Please Enter a Zip', trigger: 'blur' },
+          { min: 5, message: 'Zip Must Be 5 or More Digits', trigger: 'blur' }
+        ]
       }
     };
   },
