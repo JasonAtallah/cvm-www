c4c43e13863f47690d8faeb82311430c5ea5e81b
Jason Atallah <jasonatallah@jasons-mbp.attlocal.net>
2018-01-30 20:59:36 -0800

    switched profile settings form grid to use bootstrap instead of element

--- src/apps/buyer/components/settings/detail/Profile.vue
+++ src/apps/buyer/components/settings/detail/Profile.vue
@@ -1,3 +1,5 @@
+
+
 <template>
   <div>
     <h3>Profile</h3>
@@ -5,60 +7,63 @@
       Update your personal and company information.
     </p>
     <div class="card card-body bg-light">
-      <div class="form-group">
+      <ElForm :model="profile.contact" :rules="rules" ref="profile.contact">
         <div class="row">
           <div class="col-sm-6">
-            <label class="settings-input-label" for="firstName">First Name</label>
-            <ElInput id="firstName" v-model="profile.contact.firstName" placeholder="First" />
+            <ElFormItem label="First Name" prop="firstName">
+              <ElInput v-model="profile.contact.firstName" placeholder="First Name" />
+            </ElFormItem>
           </div>
           <div class="col-sm-6">
-            <label class="settings-input-label" for="lastName">Last Name</label>
-            <ElInput id="lastName" v-model="profile.contact.lastName" placeholder="Last" />
+            <ElFormItem label="Last Name" prop="lastName">
+              <ElInput v-model="profile.contact.lastName" placeholder="Last Name" />
+            </ElFormItem>
           </div>
         </div>
-        <br>
         <div class="row">
           <div class="col-sm-6">
-            <label class="settings-input-label" for="phone">Phone</label>
-            <ElInput id="phone" v-model="profile.contact.phone" placeholder="Phone" />
+            <ElFormItem label="Phone" prop="phone">
+              <ElInput v-model="profile.contact.phone" placeholder="Phone" />
+            </ElFormItem>
           </div>
           <div class="col-sm-6">
-            <label class="settings-input-label" for="email">Email</label>
-            <ElInput id="email" v-model="profile.contact.email" placeholder="Email" />
-          </div>
-        </div><br>
-        <div class="row">
-          <div class="col-sm-12">
-            <h4>Company</h4>
+            <ElFormItem label="Email" prop="email">
+              <ElInput v-model="profile.contact.email" placeholder="Email" />
+            </ElFormItem>
           </div>
         </div>
-        <br>
+      </ElForm>
+      <ElForm :model="profile.company" :rules="rules" ref="profile.company">
         <div class="row">
           <div class="col-sm-6">
-            <label class="settings-input-label" for="name">Name</label>
-            <ElInput id="name" v-model="profile.company.name" placeholder="Name" />
+            <ElFormItem label="Name" prop="name">
+              <ElInput v-model="profile.company.name" placeholder="Name" />
+            </ElFormItem>
           </div>
           <div class="col-sm-6">
-            <label class="settings-input-label" for="address">Address</label>
-            <ElInput id="address" v-model="profile.company.address" placeholder="Address" />
+            <ElFormItem label="Address" prop="address">
+              <ElInput v-model="profile.company.address" placeholder="Address" />
+            </ElFormItem>
           </div>
         </div>
-        <br>
         <div class="row">
           <div class="col-sm-4">
-            <label class="settings-input-label" for="city">City</label>
-            <ElInput id="city" v-model="profile.company.city" placeholder="City" />
+            <ElFormItem label="City" prop="city">
+              <ElInput v-model="profile.company.city" placeholder="City" />
+            </ElFormItem>
           </div>
           <div class="col-sm-4">
-            <label class="settings-input-label" for="state">State</label>
-            <ElInput id="state" v-model="profile.company.state" placeholder="State" />
+            <ElFormItem label="State" prop="state">
+              <ElInput v-model="profile.company.state" placeholder="State" />
+            </ElFormItem>
           </div>
           <div class="col-sm-4">
-            <label class="settings-input-label" for="zip">Zip</label>
-            <ElInput id="zip" v-model="profile.company.zip" placeholder="Zip" />
+            <ElFormItem label="Zip" prop="zip">
+              <ElInput v-model="profile.company.zip" placeholder="Zip Code" />
+            </ElFormItem>
           </div>
         </div>
-     </div>
+      </ElForm>
     </div>
 
     <div class="modal-footer">
@@ -72,20 +77,20 @@
 <script>
 import {
   Button as ElButton,
+  Col as ElCol,
+  Form as ElForm,
+  FormItem as ElFormItem,
   Input as ElInput,
   Notification } from 'element-ui';
 
 export default {
-  props: ['buyer'],
   components: {
     ElButton,
+    ElCol,
+    ElForm,
+    ElFormItem,
     ElInput,
     Notification
-  },
-  computed: {
-    canNotUpdate() {
-      return _.isEqual(this.buyer.profile, this.profile);
-    }
   },
   data() {
     return {
@@ -103,20 +108,72 @@ export default {
           phone: this.buyer.profile.contact.phone || null,
           email: this.buyer.profile.contact.email || null
         }
+      },
+      rules: {
+        firstName: [
+          { required: true, message: 'Please Enter First Name', trigger: 'blur' }
+        ],
+        lastName: [
+          { required: true, message: 'Please Enter Last Name', trigger: 'blur' }
+        ],
+        phone: [
+          { required: true, message: 'Please Enter Phone Number', trigger: 'blur' },
+          { min: 7, message: 'Number Should Be At Least 7 Digits', trigger: 'blur' }
+        ],
+        email: [
+          { required: true, message: 'Please Input Email Address', trigger: 'blur' },
+          { type: 'email', message: 'Please Input Valid Email Address', trigger: 'blur,change' }
+        ],
+        name: [
+          { required: true, message: 'Please Enter a Company Name', trigger: 'blur' }
+        ],
+        address: [
+          { required: true, message: 'Please Enter an Address', trigger: 'blur' }
+        ],
+        city: [
+          { required: true, message: 'Please Enter a City', trigger: 'blur' }
+        ],
+        state: [
+          { required: true, message: 'Please Enter a State', trigger: 'blur' },
+          { min: 2, max: 2, message: 'State Must Be Two Letters', trigger: 'blur' }
+        ],
+        zip: [
+          { required: true, message: 'Please Enter a Zip', trigger: 'blur' },
+          { min: 5, message: 'Zip Must Be 5 or More Digits', trigger: 'blur' }
+        ]
       }
     };
+  },
+  props: ['buyer'],
+  computed: {
+    canNotUpdate() {
+      return _.isEqual(this.buyer.profile, this.profile);
+    }
   },
   methods: {
     cancel() {
       this.profile = _.cloneDeep(this.buyer.profile);
     },
     updateProfile() {
-      this.$store.dispatch('updateBuyerProfile', _.cloneDeep(this.profile));
-      Notification({
-        title: 'Success',
-        message: 'Profile Updated!',
-        type: 'success',
-        duration: 2000
+      this.$refs['profile.contact'].validate((valid) => {
+        this.$refs['profile.company'].validate((valid) => {
+          if (valid) {
+            this.$store.dispatch('updateBuyerProfile', _.cloneDeep(this.profile));
+            Notification({
+              title: 'Success',
+              message: 'Profile Updated!',
+              type: 'success',
+              duration: 2000
+            });
+          } else {
+            Notification({
+              title: 'Uh oh',
+              message: 'Something Went Wrong',
+              type: 'error',
+              duration: 2000
+            });
+          }
+        });
       });
     }
   }
