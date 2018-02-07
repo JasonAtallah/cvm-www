454ded330f7bc2ef788bd2662816adb2fce8bb6b
Jason Atallah <jasonatallah@jasons-mbp.attlocal.net>
2018-02-06 22:02:12 -0800

    converted SendTimes detail to use Detail component

--- src/components/buyer/vendors/detail/SendTimes.vue
+++ src/components/buyer/vendors/detail/SendTimes.vue
@@ -18,139 +18,112 @@ ul.suggestedTimes button {
 </style>
 
 <template>
-  <div>
-    <h3>Propose Times to meet with {{ vendor.name }}</h3>
-    <p class="lead">
-      Invite {{ vendor.name }} to a meeting to review their products. They will receive an email inviting them to choose one of the time slots you provide below. You will be notified when they make their selection.
-    </p>
-
-    <div class="card card-body bg-light">
-      <div class="row">
-        <div class="col-sm-12">
-
-          <div class="row">
-            <div class="col-sm-6">
-              <div class="form-group row">
-                <label for="location" class="col-sm-4 col-form-label">Location:</label>
-                <div class="col-sm-6">
-                  <ElSelect v-model="location" clearable @change="onLocationChange" placeholder="Choose a location">
-                    <ElOption v-for="(location, index) in locations" :key="index" :label="location.name" :value="index" />
-                    <ElOption key="-1" label="Add Location" value="-1">
-                      <ElButton type="text" size="medium" style="padding: 0">Add Location</ElButton>
-                    </ElOption>
-                  </ElSelect>
-                </div>
-              </div>
-            </div>
+  <Detail :title="genTitle" :description="genDescription" :canSave="canSave" @save="send" @cancel="cancel">
+    <div class="row">
+      <div class="col-sm-6">
+        <div class="form-group row">
+          <label for="location" class="col-sm-4 col-form-label">Location:</label>
+          <div class="col-sm-6">
+            <ElSelect v-model="location" clearable @change="onLocationChange" placeholder="Choose a location">
+              <ElOption v-for="(location, index) in locations" :key="index" :label="location.name" :value="index" />
+              <ElOption key="-1" label="Add Location" value="-1">
+                <ElButton type="text" size="medium" style="padding: 0">Add Location</ElButton>
+              </ElOption>
+            </ElSelect>
           </div>
-
-          <div class="row">
-            <div class="col-sm-6">
-              <div class="form-group row">
-                <label for="startDate" class="col-sm-4 col-form-label">Date:</label>
-                <div class="col-sm-8">
-                  <ElDatePicker
-                    v-model="startDate"
-                    :format="datePicker.format"
-                    :type="datePicker.type" />
-                </div>
-              </div>
-            </div>
-          </div>
-
-          <div class="row">
-            <div class="col-sm-6">
-              <div class="form-group row">
-                <label for="startTime" class="col-sm-4 col-form-label">Time:</label>
-                <div class="col-sm-8">
-                  <ElTimeSelect
-                    v-model="startTime"
-                    :picker-options="timePicker" />
-                </div>
-              </div>
-            </div>
-          </div>
-
-          <div class="row">
-            <div class="col-sm-6">
-              <div class="form-group row">
-                <label for="duration" class="col-sm-4 col-form-label">Duration (mins):</label>
-                <div class="col-sm-3">
-                  <ElInputNumber v-model="duration" controlsPosition="right" />
-                </div>
-              </div>
-            </div>
-          </div>
-
-          <div class="row">
-            <div class="col-sm-6">
-              <div class="form-group">
-
-              </div>
-            </div>
-          </div>
-
-          <div class="row">
-            <div class="col-sm-6">
-              <div class="form-group">
-                <button type="button" class="btn btn-primary" @click.prevent="addTime" :disabled="!canAddTime">Add Time</button>
-              </div>
-            </div>
-          </div>
-
-          <div class="row">
-            <div class="col-sm-12">
-              <br /><br />
-              <h4>Added Times</h4>
-              <br />
-            </div>
-          </div>
-
-          <div class="row">
-            <div class="col-sm-12">
-              <div v-if="suggestedTimes.length === 0">
-                No times added yet
-              </div>
-              <ul class="suggestedTimes">
-                <li v-for="(time, index) in suggestedTimes" :key="index">
-                  <div class="row">
-                    <div class="col-sm-12">
-                      <i class="el-icon-time"></i>
-                      <span>{{ formatDate(time.startDate) }} to {{ formatTime(getEndDate(time)) }}</span>
-                    </div>
-                  </div>
-                  <div class="row">
-                    <div class="col-sm-2">
-                      <i class="el-icon-location-outline"></i>
-                      <span>{{ time.location.name }}</span>
-                    </div>
-                  </div>
-                  <div class="row">
-                    <div class="col-sm-2">
-                      <i class="el-icon-delete"></i>
-                      <button type="button" class="btn btn-link" @click.prevent="removeTime(index)">Remove</button>
-                    </div>
-                  </div>
-                </li>
-              </ul>
-            </div>
-          </div>
-
         </div>
-
       </div>
     </div>
 
-    <div class="modal-footer">
-      <button type="button" class="btn btn-lg btn-primary" @click.prevent="send" :disabled="!canSubmit">Send</button>
-      <button type="button" class="btn btn-lg btn-default" @click.prevent="cancel">Cancel</button>
+    <div class="row">
+      <div class="col-sm-6">
+        <div class="form-group row">
+          <label for="startDate" class="col-sm-4 col-form-label">Date:</label>
+          <div class="col-sm-8">
+            <ElDatePicker
+              v-model="startDate"
+              :format="datePicker.format"
+              :type="datePicker.type" />
+          </div>
+        </div>
+      </div>
     </div>
 
-  </div>
+    <div class="row">
+      <div class="col-sm-6">
+        <div class="form-group row">
+          <label for="startTime" class="col-sm-4 col-form-label">Time:</label>
+          <div class="col-sm-8">
+            <ElTimeSelect
+              v-model="startTime"
+              :picker-options="timePicker" />
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="row">
+      <div class="col-sm-6">
+        <div class="form-group row">
+          <label for="duration" class="col-sm-4 col-form-label">Duration (mins):</label>
+          <div class="col-sm-3">
+            <ElInputNumber v-model="duration" controlsPosition="right" />
+          </div>
+        </div>
+      </div>
+    </div>
+    <br>
+    <div class="row">
+      <div class="col-sm-6">
+        <div class="form-group">
+          <button type="button" class="btn btn-primary" @click.prevent="addTime" :disabled="!canAddTime">Add Time</button>
+        </div>
+      </div>
+    </div>
+
+    <div class="row">
+      <div class="col-sm-12">
+        <br /><br />
+        <h4>Added Times</h4>
+        <br />
+      </div>
+    </div>
+
+    <div class="row">
+      <div class="col-sm-12">
+        <div v-if="suggestedTimes.length === 0">
+          No times added yet
+        </div>
+        <ul class="suggestedTimes">
+          <li v-for="(time, index) in suggestedTimes" :key="index">
+            <div class="row">
+              <div class="col-sm-12">
+                <i class="el-icon-time"></i>
+                <span>{{ formatDate(time.startDate) }} to {{ formatTime(getEndDate(time)) }}</span>
+              </div>
+            </div>
+            <div class="row">
+              <div class="col-sm-2">
+                <i class="el-icon-location-outline"></i>
+                <span>{{ time.location.name }}</span>
+              </div>
+            </div>
+            <div class="row">
+              <div class="col-sm-2">
+                <i class="el-icon-delete"></i>
+                <button type="button" class="btn btn-link" @click.prevent="removeTime(index)">Remove</button>
+              </div>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </Detail>
 </template>
 
 <script>
 import { mapGetters } from 'vuex';
+import Detail from '@/components/masterDetail/Detail';
 import moment from 'moment';
 import {
   Button as ElButton,
@@ -162,6 +135,7 @@ import {
 
 export default {
   components: {
+    Detail,
     ElButton,
     ElDatePicker,
     ElInputNumber,
@@ -197,8 +171,14 @@ export default {
     canAddTime() {
       return this.location !== null && this.startDate && this.startTime;
     },
-    canSubmit() {
+    canSave() {
       return this.suggestedTimes.length;
+    },
+    genDescription() {
+      return `Invite ${this.vendor.name} to a meeting to review their products. They will receive an email inviting them to choose one of the time slots you provide below. You will be notified when they make their selection.`;
+    },
+    genTitle() {
+      return `Propose Times to meet with ${this.vendor.name}`;
     },
     timeWarning() {
       return '';
