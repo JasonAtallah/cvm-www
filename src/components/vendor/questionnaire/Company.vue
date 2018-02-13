20bbbc5bcfcb7d67d099150e691ce5ec5f1be703
Jason Atallah <jasonatallah@jasons-mbp.attlocal.net>
2018-02-12 22:52:00 -0800

    fixed questionnaire to query fields off id NOT name

--- src/components/vendor/questionnaire/Company.vue
+++ src/components/vendor/questionnaire/Company.vue
@@ -2,48 +2,48 @@
 <Detail title="Company Information" :showSave="false" :showCancel="false">
   <ElForm :model="response" :rules="company" ref="response.company">
     <div class="row">
-      <div class="col-sm-12 col-md-6" v-if="showField('Name')">
+      <div class="col-sm-12 col-md-6" v-if="showField('name')">
         <ElFormItem label="Name" prop="name">
           <ElInput v-model="response.name" placeholder="Name" />
         </ElFormItem>
       </div>
-      <div class="col-sm-12 col-md-6" v-if="showField('Website')">
+      <div class="col-sm-12 col-md-6" v-if="showField('website')">
         <ElFormItem label="Website" prop="website">
           <ElInput v-model="response.website" placeholder="Website" />
         </ElFormItem>
       </div>
     </div>
     <div class="row">
-      <div class="col-sm-12" v-if="showField('Address')">
+      <div class="col-sm-12" v-if="showField('address')">
         <ElFormItem label="Address" prop="address">
           <ElInput v-model="response.address" placeholder="Address" />
         </ElFormItem>
       </div>
     </div>
     <div class="row">
-      <div class="col-sm-12 col-md-6" v-if="showField('City')">
+      <div class="col-sm-12 col-md-6" v-if="showField('city')">
         <ElFormItem label="City" prop="city">
           <ElInput v-model="response.city" placeholder="City" />
         </ElFormItem>
       </div>
-      <div class="col-sm-6 col-md-3" v-if="showField('State')">
+      <div class="col-sm-6 col-md-3" v-if="showField('state')">
         <ElFormItem label="State" prop="state">
           <ElInput v-model="response.state" placeholder="State" />
         </ElFormItem>
       </div>
-      <div class="col-sm-6 col-md-3" v-if="showField('Zip')">
+      <div class="col-sm-6 col-md-3" v-if="showField('zip')">
         <ElFormItem label="Zip" prop="zip">
           <ElInput v-model="response.zip" placeholder="Zip" />
         </ElFormItem>
       </div>
     </div>
     <div class="row">
-      <div class="col-sm-12 col-md-6" v-if="showField('SREA')">
+      <div class="col-sm-12 col-md-6" v-if="showField('srea')">
         <ElFormItem label="SREA#" prop="srea">
           <ElInput v-model="response.srea" placeholder="SREA" />
         </ElFormItem>
       </div>
-      <div class="col-sm-12 col-md-2" v-if="showField('HaveTaxId')">
+      <div class="col-sm-12 col-md-2" v-if="showField('haveTaxId')">
         <ElFormItem label="Have Tax ID" prop="selection">
           <ElSelect v-model="response.haveTaxId" placeholder="Select">
             <ElOption :value="false" label="No" />
@@ -51,7 +51,7 @@
           </ElSelect>
         </ElFormItem>
       </div>
-      <div class="form-group col-sm-12 col-md-2" v-if="showField('HaveSellerPermit')">
+      <div class="form-group col-sm-12 col-md-2" v-if="showField('haveSellerPermit')">
         <ElFormItem label="Have Seller Permit" prop="selection">
           <ElSelect v-model="response.haveSellerPermit" placeholder="Select">
             <ElOption :value="false" label="No" />
@@ -59,7 +59,7 @@
           </ElSelect>
         </ElFormItem>
       </div>
-      <div class="form-group col-sm-12 col-md-2" v-if="showField('PartOfCollective')">
+      <div class="form-group col-sm-12 col-md-2" v-if="showField('partOfCollective')">
         <ElFormItem label="Part of Collective" prop="selection">
           <ElSelect v-model="response.partOfCollective" placeholder="Select">
             <ElOption :value="false" label="No" />
@@ -102,7 +102,7 @@ export default {
   props: ['questions', 'response'],
   methods: {
     getField(fieldName) {
-      return _.find(this.questions, { name: fieldName });
+      return _.find(this.questions, { id: fieldName });
     },
     showField(fieldName) {
       return this.getField(fieldName).enabled;
--- src/components/vendor/questionnaire/Contact.vue
+++ src/components/vendor/questionnaire/Contact.vue
@@ -2,26 +2,9 @@
 <Detail title="Contact Information" :showSave="false" :showCancel="false">
   <ElForm :model="response" :rules="contact" ref="response.contact">
     <div class="row">
-      <div class="form-group col-sm-12 col-md-6" v-if="showField('FirstName')">
-        <ElFormItem label="First Name" prop="firstName">
-          <ElInput v-model="response.firstName" placeholder="First Name" />
-        </ElFormItem>
-      </div>
-      <div class="form-group col-sm-12 col-md-6" v-if="showField('LastName')">
-        <ElFormItem label="Last Name" prop="lastName">
-          <ElInput v-model="response.lastName" placeholder="Last Name" />
-        </ElFormItem>
-      </div>
-    </div>
-    <div class="row">
-      <div class="form-group col-sm-12 col-md-6" v-if="showField('Email')">
-        <ElFormItem label="Email" prop="email">
-          <ElInput v-model="response.email" placeholder="Email" />
-        </ElFormItem>
-      </div>
-      <div class="form-group col-sm-12 col-md-6" v-if="showField('Phone')">
-        <ElFormItem label="Phone" prop="phone">
-          <ElInput v-model="response.phone" placeholder="Phone" />
+      <div class="col-sm-12 col-md-6" v-for="field in questions" :key="field.id" v-if="showField(field.id)">
+        <ElFormItem :label="field.name" :prop="field.id">
+          <ElInput v-model="response[field.name]" :placeholder="field.name" />
         </ElFormItem>
       </div>
     </div>
@@ -53,7 +36,7 @@ export default {
   props: ['questions', 'response'],
   methods: {
     getField(fieldName) {
-      return _.find(this.questions, { name: fieldName });
+      return _.find(this.questions, { id: fieldName });
     },
     showField(fieldName) {
       return this.getField(fieldName).enabled;
